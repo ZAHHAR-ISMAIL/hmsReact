3e8ca7c6d5a789e2a0dc5e6187a2324da98c96f6
ZAHHAR-ISMAIL <zahhar.ismail@gmail.com>
2022-03-02 15:57:44 +0100

    first

---
+++ App.js
@@ -0,0 +1,150 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {
+  HmsPushInstanceId,
+  HmsPushEvent
+} from "@hmscore/react-native-hms-push";
+
+HmsPushInstanceId.getToken('')
+      .then((result) => {
+        console.log('getToken', result);
+      })
+      .catch((err) => {
+        alert('[getToken] Error/Exception: ' + JSON.stringify(err));
+});
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+
+  useEffect(() => {
+   
+    onTokenReceivedListener = HmsPushEvent.onTokenReceived((result) => {
+      console.log('onTokenReceived', result);
+    });
+    
+    onTokenErrorListener = HmsPushEvent.onTokenError((result) => {
+      console.log('onTokenError', result);
+    });
+    
+    onNotificationOpenedAppListener = HmsPushEvent.onNotificationOpenedApp(
+      (result) => {
+        console.log('onNotificationOpenedAppListener', result);
+      },
+    );
+
+    () => {
+      onTokenReceivedListener.remove();
+      onTokenErrorListener.remove();
+      onNotificationOpenedAppListener.remove();
+    };
+  }, []);
+
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            BBBBBBBBBBBBBBB <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
